fd1edf05d2bfad9b17b84d4b1aa08f458589894b
Nagybhe Hage <65202520+nagybhe@users.noreply.github.com>
2021-06-28 12:25:39 -0300

    php

---
+++ bot.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat Bot</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="title">Chat Bot</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Sou um Bot,posso te ajudar?</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Digite algo aqui .." required>
+                <button id="send-btn">Enviar</button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $("#send-btn").on("click", function() {
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>' + $value + '</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+
+                // iniciar código ajax
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text=' + $value,
+                    success: function(result) {
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>' + result + '</p></div></div>';
+                        $(".form").append($replay);
+                        // quando o chat desce, a barra de rolagem automaticamente desce para a parte inferior
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
+
+</body>
+
+</html>
